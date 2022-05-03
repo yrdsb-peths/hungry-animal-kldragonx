532772c43d8431f2e474bcc05583c7f2c2e8110a
kldragonx <101230930+kldragonx@users.noreply.github.com>
2022-05-03 10:14:31 -0400

    Add files via upload
    does this work?

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,12 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels
+        
+        
+        super(600, 400, 1);
+        addObject(new rhino(), 300, 200);
+        
+        
     }
-}
+} 
---
+++ rhino.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class rhino here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class rhino extends Actor
+{
+    /**
+     * Act - do whatever the rhino wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(4);// Add your action code here.
+    }
+}
